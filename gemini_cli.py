24f506a92567fb72bb7b21b30d077448de14c7e1
simon <hesynote@gmail.com>
2026-03-27 16:00:02 +0800

    add highlight syntax for tool call output

--- gemini_cli.py
+++ gemini_cli.py
@@ -554,7 +554,7 @@ class GeminiCliCommand(sublime_plugin.WindowCommand):
         chat_view = self.window.new_file()
         chat_view.set_name(CHAT_VIEW_NAME)
         chat_view.set_scratch(True)
-        chat_view.set_syntax_file("Packages/Markdown/Markdown.sublime-syntax")
+        chat_view.set_syntax_file("Packages/GeminiCLI/GeminiChat.sublime-syntax")
         chat_view.settings().set("draw_minimap", False)
         chat_view.settings().set("line_numbers", False)
         chat_view.settings().set("word_wrap", True)
